206e0c95f85ebef2df40d00350ba183853f806e9
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-06-25 10:27:42 -0400

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+package main.java;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -7,3 +10,14 @@ public class Main {
 
     }
 }
+    public static void main(String[] args) {
+        //String line;
+        Scanner in= new Scanner (system.in);
+        System.out.print("Hello Alice");
+        line= in.nextline();
+        System.out.printIn( )
+
+
+
+
+    }
