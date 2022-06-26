e224068e2396b9575575fe4864a79191358c7af4
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-06-26 14:11:09 -0400

    Hi Alice and Bob assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,33 @@
-
-package main.java;
-
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
+        List nameList = new ArrayList();
+        JFrame frame = new JFrame("Main Window");
+        JOptionPane.showMessageDialog(frame, "Hi Alice and Bob!", "Error", JOptionPane.ERROR_MESSAGE);
+
+
+        }
 
     }
-}
-    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+    /*public static void main(String[] args) {
         //String line;
         Scanner in= new Scanner (system.in);
         System.out.print("Hello Alice");
         line= in.nextline();
         System.out.printIn( )
-
-
-
-
-    }
+ }*/
