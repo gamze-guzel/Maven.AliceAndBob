4377393e12d9f215ad907ff2e40278dd34ad54cf
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-06-26 15:11:23 -0400

    Yay it worked

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,27 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args ) {
-        List nameList = new ArrayList();
-        JFrame frame = new JFrame("Main Window");
-        JOptionPane.showMessageDialog(frame, "Hi Alice and Bob!", "Error", JOptionPane.ERROR_MESSAGE);
+        JTextField nameField1 = new JTextField(15);
+        JTextField nameField2 = new JTextField(15);
 
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Name 1:"));
+        myPanel.add(nameField1);
+        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        myPanel.add(new JLabel("Name 2:"));
+        myPanel.add(nameField2);
 
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Please Enter Your Names", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            if ((nameField1.getText().equals("Alice") || nameField1.getText().equals("Bob")) && (nameField2.getText().equals("Alice") || nameField2.getText().equals("Bob"))) {
+                System.out.println("Hi " + nameField1.getText() + " and " + nameField2.getText());
+            }
+            else {
+                return;
+            }
         }
-
+        }
     }
 
 
